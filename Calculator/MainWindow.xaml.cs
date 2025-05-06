2b9d3b6b241bddcffdaa88d7c49d92d3ca6639c1
212311 <212311@1-1220-08>
2025-05-06 14:42:46 +0300

    WIN

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Runtime.Remoting.Channels;
 using System.Text;
@@ -34,7 +35,22 @@ namespace Calculator
         private void One_Click(object sender, RoutedEventArgs e)
         {
             Button button = (Button)sender;
-            currentInput += button.Content.ToString(); 
+            string digit = button.Content.ToString();
+
+            if (currentInput == "0" && digit != "0")
+            {
+                currentInput = digit;
+            }
+            else if (currentInput == "-0")
+            {
+                currentInput = "-" + digit;
+            }
+           
+            else
+            {
+                currentInput += digit;
+            }
+
             numbersTbl.Text = currentInput;
         }
 
@@ -50,63 +66,150 @@ namespace Calculator
             Button button = (Button)sender;
             string operatorSymbol = button.Content.ToString();
 
-            if (operatorSymbol == "-" && string.IsNullOrEmpty(currentInput) && firstOperand == null)
+            
+            if (operatorSymbol == "-" && string.IsNullOrEmpty(currentInput))
             {
                 currentInput = "-";
                 numbersTbl.Text = currentInput;
-                return; 
+                return;
+            }
+
+            if (currentInput == "-" || currentInput == ".")
+            {
+                return;
+            }
+
+          
+            if (!string.IsNullOrEmpty(currentInput) && currentInput.EndsWith("."))
+            {
+                currentInput = currentInput.TrimEnd('.'); 
+            }
+
+          
+            if (string.IsNullOrEmpty(currentInput) && !firstOperand.HasValue)
+            {
+                return;
             }
 
             
             if (!string.IsNullOrEmpty(currentInput))
             {
-                firstOperand = double.Parse(currentInput);
-                operation = operatorSymbol[0];
-                currentInput = "";
-                numbersTbl.Text = "";
+                try
+                {
+                    
+                    string numberToParse = currentInput.Replace(".",
+                        CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+
+                    firstOperand = double.Parse(numberToParse);
+                    operation = operatorSymbol[0];
+                    currentInput = ""; 
+                }
+                catch (FormatException)
+                {
+                    numbersTbl.Text = "Error";
+                    ResetCalculator();
+                    return;
+                }
             }
-            
-            else if (firstOperand.HasValue)
+            else if (firstOperand.HasValue) 
             {
                 operation = operatorSymbol[0];
             }
         }
 
-
         private void Equally_Click(object sender, RoutedEventArgs e)
         {
+           
             if (firstOperand.HasValue && operation.HasValue && !string.IsNullOrEmpty(currentInput))
             {
-                double secondOperand = double.Parse(currentInput);
-                double result = 0;
-
-                switch (operation)
+                try
                 {
-                    case '+':
-                        result = firstOperand.Value + secondOperand;
-                        break;
-                    case '-':
-                        result = firstOperand.Value - secondOperand;
-                        break;
-                    case '*':
-                        result = firstOperand.Value * secondOperand;
-                        break;
-                    case '/':
-                        if (secondOperand == 0)
-                        {
-                            numbersTbl.Text = "Error";
-                            return;
-                        }
-                        result = firstOperand.Value / secondOperand;
-                        break;
+                  
+                    string numberString = currentInput.Replace(".",
+                        CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+
+                    double secondOperand = double.Parse(numberString);
+                    double result = 0;
+                    bool calculationError = false;
+
+                    switch (operation.Value)
+                    {
+                        case '+':
+                            result = firstOperand.Value + secondOperand;
+                            break;
+                        case '-':
+                            result = firstOperand.Value - secondOperand;
+                            break;
+                        case '*':
+                            result = firstOperand.Value * secondOperand;
+                            break;
+                        case '/':
+                            if (secondOperand == 0)
+                            {
+                                numbersTbl.Text = "Error: Division by zero";
+                                calculationError = true;
+                            }
+                            else
+                            {
+                                result = firstOperand.Value / secondOperand;
+                            }
+                            break;
+                        default:
+                            numbersTbl.Text = "Error: Invalid operation";
+                            calculationError = true;
+                            break;
+                    }
+
+                    if (!calculationError)
+                    {
+                        numbersTbl.Text = result.ToString(CultureInfo.InvariantCulture);
+                        firstOperand = result;
+                        currentInput = "";
+                    }
+                    else
+                    {
+                        ResetCalculator();
+                    }
                 }
+                catch (FormatException)
+                {
+                    numbersTbl.Text = "Error: Invalid number";
+                    ResetCalculator();
+                }
+                catch (OverflowException)
+                {
+                    numbersTbl.Text = "Error: Too large";
+                    ResetCalculator();
+                }
+            }
+        }
 
-                numbersTbl.Text = result.ToString();
-                firstOperand = result;  
-                currentInput = "";
-                operation = null;
+        private void ResetCalculator()
+        {
+            firstOperand = null;
+            operation = null;
+            currentInput = "";
+        }
+        private void DecimalPoint_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (string.IsNullOrEmpty(currentInput))
+            {
+                currentInput = "0.";
+            }
+           
+            else if (currentInput == "-")
+            {
+                currentInput = "-0.";
+            }
+           
+            else if (!currentInput.Contains("."))
+            {
+                currentInput += ".";
             }
 
-            }
+            numbersTbl.Text = currentInput;
+        }
+
     }
 }
